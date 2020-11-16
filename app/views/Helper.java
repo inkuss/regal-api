d96c9a53dd3ff910c140570b8c0ac74c67ea1289
Ingolf Kuss <kuss@hbz-nrw.de>
2020-11-16 18:39:24 +0100

    treat case label==null, EDOZWO-1032

--- app/views/Helper.java
+++ app/views/Helper.java
@@ -208,6 +208,27 @@ public class Helper {
 					result2.add(subject);
 				} else {
 					play.Logger.debug("label == null");
+					String uri = c.at("/@id").asText();
+					if (uri.contains("rpb#nr")) {
+						/**
+						 * Schlagworte mit diesem prefix kommen nicht zur Anzeige
+						 */
+						continue;
+					}
+					String sourceId = c.at("/source/0/@id").asText();
+					String source = c.at("/source/0/label").asText();
+					String notation = c.at("/notation").asText();
+
+					if (uri == null || uri.isEmpty()) {
+						/**
+						 * Wenn keine URI vorhanden ist, kann das label zur Suche benutzt
+						 * werden. Der Suchstring wird unter
+						 * views/tags/resourceView#displaySubject gebildet.
+						 */
+						uri = label;
+					}
+					play.Logger.debug("sourceId=" + sourceId + ", uri=" + uri
+							+ ", notation=" + notation);
 				}
 			}
 		}
